375a3c9a0f9bcaae127bd5d5122b29743bc2e657
Alexander Minkin <amink@mail.ru>
2022-02-09 11:23:37 +0300

    plotbasis.m fix bug with dotted and bold lines

--- octave/plotbasis.m
+++ octave/plotbasis.m
@@ -3,17 +3,27 @@ for i=0:n;
   s = ['basis_',int2str(i),'.dat'];
   pseg=load(s);
   p = plot(pseg(:,1),pseg(:,2));
-  set(p, "linewidth", 2)  
+  set(p, "linewidth", 2)
 
   dotted_array = [];
   ##  dotted_array = [1 2 4];
 
-  Na = size(dotted_array,2);
-  for j=1:Na;    
-    if i==j;
+  Nd = size(dotted_array,2);
+  for j=1:Nd;
+    if i==dotted_array(j);
       set(p, "linestyle", '--')
     end;
   end;
+  
+  bold_array = [];
+  ##  bold_array = [0 6];
+  Nb = size(bold_array,2);
+  for j=1:Nb;
+    if i==bold_array(j);
+      set(p, "linewidth", 3)
+    end;
+  end;
+  
   h=get(gcf, "currentaxes");
   set(h, "fontsize", 24, 'FontName', 'Times New Roman')
   hold on;
